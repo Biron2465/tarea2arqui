9ff9c82c0be474120fbdb24052f2730089d3db52
Biron Aguilar Rodríguez <biron2465@gmail.com>
2024-10-31 21:35:02 -0600

    Update Comments.vue

--- components/Comments.vue
+++ components/Comments.vue
@@ -1,30 +1,29 @@
 <template>
-    <div id="comments-section" class="comments-section"></div>
-  </template>
-  
-  <script setup>
-  import { onMounted, onUnmounted } from 'vue'
-  
-  onMounted(() => {
-    console.log("Comments component mounted");
-  
-    const existingScript = document.getElementById('comments-script');
-    if (existingScript) existingScript.remove();
-  
-    const script = document.createElement('script');
-    script.src = "https://utteranc.es/client.js";
-    script.id = 'comments-script';
-    script.setAttribute('repo', "NanaCs08/Tarea2_Arqui");
-    script.setAttribute('issue-term', "pathname");
-    script.setAttribute('theme', "github-dark");
-    script.setAttribute('crossorigin', "anonymous");
-    script.async = true;
-    document.getElementById('comments-section').appendChild(script);
-  });
-  
-  onUnmounted(() => {
-    const script = document.getElementById('comments-script');
-    if (script) script.remove();
-  });
-  </script>
-  
+  <div id="comments-section" class="comments-section"></div>
+</template>
+
+<script setup>
+import { onMounted, onUnmounted } from 'vue'
+
+onMounted(() => {
+  console.log("Comments component mounted");
+
+  const existingScript = document.getElementById('comments-script');
+  if (existingScript) existingScript.remove();
+
+  const script = document.createElement('script');
+  script.src = "https://utteranc.es/client.js";
+  script.id = 'comments-script';
+  script.setAttribute('repo', "Biron2465/tarea2arqui");
+  script.setAttribute('issue-term', "pathname");
+  script.setAttribute('theme', "github-dark");
+  script.setAttribute('crossorigin', "anonymous");
+  script.async = true;
+  document.getElementById('comments-section').appendChild(script);
+});
+
+onUnmounted(() => {
+  const script = document.getElementById('comments-script');
+  if (script) script.remove();
+});
+</script>
